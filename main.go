9b1601602db0875a3b9097eed45a8d291702d145
sergejdodig <sergej.dodig@gmail.com>
2021-01-25 08:46:08 +0100

    w2

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func init() {
 	fmt.Println("http://localost:8002/devicedata/")
 	fmt.Println("*************************")
+	fmt.Println("Server startovan")
+	fmt.Println("*************************")
 }
 
 func main() {
